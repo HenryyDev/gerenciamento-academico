7649f5f9ff397fc54f57632f640c63a335812675
henry4i20 <henrimito1305@gmail.com>
2025-03-27 11:21:38 -0300

    update semana 3

--- main.py
+++ main.py
@@ -14,119 +14,56 @@ def main():
         ["4","Excluir"],
         ["9","Voltar ao menu principal"],
     ]
+    while True:
+      print("+-----------MENU PRINCIPAL------------+\n")
 
-    print("+-----------MENU PRINCIPAL------------+\n")
+      for opcoes_menu_principal in opcao_menu_principal:
+         print(f"| ({opcoes_menu_principal[0]:<1})  {opcoes_menu_principal[1]:<30} |")
+      
+      print("\n+-------------------------------------+")
+      try:
+         opcao_selecionada=int(input("escolha a opção desejada:"))
+      except ValueError:
+          print("Opção inválida. Por favor, insira um número.")
+          continue
 
-    for opcoes_menu_principal in opcao_menu_principal:
-        print(f"| ({opcoes_menu_principal[0]:<1})  {opcoes_menu_principal[1]:<30} |")
-    
-    print("\n+-------------------------------------+")
+      if opcao_selecionada==9:
+         print("Saindo do programa...")
+         break
 
-    opcao_selecionada=int(input("escolha a opção desejada:"))
-
-    if opcao_selecionada==1:
-        print(f"\n****[ESTUDANTES] MENU DE OPERAÇÕES*****\n")
-
-        for opcoes_crud in opcao_crud:
-            print(f"| ({opcoes_crud[0]:<1})  {opcoes_crud[1]:<30} |")
-        print("\n+-------------------------------------+")
-
-        acao_desejada=int(input("Informe a ação desejada: "))
-        if acao_desejada==1:
-           print("\n========== INCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==2:
-           print("\n========== LISTANDO ===================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==3:
-           print("\n========== ATUALIZANDO ================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==4:
-           print("\n========== EXCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-
-    elif opcao_selecionada==2:
-        print(f"\n****[PROFESSORES] MENU DE OPERAÇÕES*****\n")
-        
-        for opcoes_crud in opcao_crud:
-            print(f"| ({opcoes_crud[0]:<1})  {opcoes_crud[1]:<30} |")
-        print("\n+-------------------------------------+")
-
-        acao_desejada=int(input("Informe a ação desejada: "))
-        if acao_desejada==1:
-           print("\n========== INCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==2:
-           print("\n========== LISTANDO ===================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==3:
-           print("\n========== ATUALIZANDO ================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==4:
-           print("\n========== EXCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-    
-    elif opcao_selecionada==3:
-        print(f"\n****[DISCIPLINAS] MENU DE OPERAÇÕES****\n")
-        
-        for opcoes_crud in opcao_crud:
-            print(f"| ({opcoes_crud[0]:<1})  {opcoes_crud[1]:<30} |")
-        print("\n+-------------------------------------+")
-
-        acao_desejada=int(input("Informe a ação desejada: "))
-        if acao_desejada==1:
-           print("\n========== INCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==2:
-           print("\n========== LISTANDO ===================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==3:
-           print("\n========== ATUALIZANDO ================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==4:
-           print("\n========== EXCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-    elif opcao_selecionada==4:
-        print(f"\n******[TURMAS] MENU DE OPERAÇÕES*******\n")
-        
-        for opcoes_crud in opcao_crud:
-            print(f"| ({opcoes_crud[0]:<1})  {opcoes_crud[1]:<30} |")
-        print("\n+-------------------------------------+")
-
-        acao_desejada=int(input("Informe a ação desejada: "))
-        if acao_desejada==1:
-           print("\n========== INCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==2:
-           print("\n========== LISTANDO ===================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==3:
-           print("\n========== ATUALIZANDO ================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==4:
-           print("\n========== EXCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-
-    elif opcao_selecionada==5:
-        print(f"\n****[MATRÍCULAS] MENU DE OPERAÇÕES****\n")
-        
-        for opcoes_crud in opcao_crud:
-            print(f"| ({opcoes_crud[0]:<1})  {opcoes_crud[1]:<30} |")
-        print("\n+-------------------------------------+")
-
-        acao_desejada=int(input("Informe a ação desejada: "))
-        if acao_desejada==1:
-           print("\n========== INCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==2:
-           print("\n========== LISTANDO ===================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==3:
-           print("\n========== ATUALIZANDO ================")
-           print("\nFinalizando aplicação...")
-        elif acao_desejada==4:
-           print("\n========== EXCLUINDO ==================")
-           print("\nFinalizando aplicação...")
-        
+      if opcao_selecionada in [1, 2, 3, 4, 5]:
+         
+         opcoes = ["ESTUDANTES", "PROFESSORES", "DISCIPLINAS", "TURMAS", "MATRÍCULAS"]
+         while True:
+            print(f"\n****[{opcoes[opcao_selecionada-1]}] MENU DE OPERAÇÕES*****\n")
+            
+            for opcoes_crud in opcao_crud:
+               print(f"| ({opcoes_crud[0]:<1})  {opcoes_crud[1]:<30} |")
+            print("\n+-------------------------------------+")   
+            try:
+               acao_desejada=int(input("Informe a ação desejada: "))
+               if acao_desejada == 9:
+                  break 
+               if acao_desejada==1:
+                  print("========== INCLUINDO ==================")
+                  print("Aguarde...")
+                  continue
+               elif acao_desejada==2:
+                  print("========== LISTANDO ===================")
+                  print("Aguarde...")
+                  continue
+               elif acao_desejada==3:
+                  print("========== ATUALIZANDO ================")
+                  print("Aguarde...")
+                  continue
+               elif acao_desejada==4:
+                  print("========== EXCLUINDO ==================")
+                  print("Aguarde...")
+                  continue
+            except ValueError:
+               print("Açao invalida tente novamente")
+           
+      
+         
         
 main()
